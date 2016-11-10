2ddf7c78df425d36f7a949cabe1ecba170aaa88a
Anniie Salvador <Anniie Salvador>
2016-11-10 13:58:17 -0600

    tabla creada con campos

--- alumnos/datos/models.py
+++ alumnos/datos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class InfoGeneral(models.Model):
+	nombre = models.CharField(max_length=50, default="Juanito")
+	apellido = models.CharField(max_length=50, default="")
+	telefono = models.CharField(max_length=50, default="")
+	fecha_nacimiento = models.DateField()
+	semestre = models.IntegerField()
+	carrera = models.CharField(max_length=50)
+	SEXO_OPTION = (
+		('F', 'Femenino'),
+		('M', 'Masculino'),
+		)
+	sexo = models.CharField(max_length=2, choices=SEXO_OPTION)
+	direccion = models.CharField(max_length=100)
+	estarura = models.CharField(max_length=100, default="")
+
+
